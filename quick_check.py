e131aa2650b80993712efd7d477b524a3ddf92a5
Youcef Kadri <youcef.d.kadri@gmail.com>
2021-05-24 22:11:59 +0000

    formatting and output changes

--- quick_check.py
+++ quick_check.py
@@ -1,40 +1,28 @@
+#!/usr/bin/env python3
 
+import os
 import json
 import subprocess
 
 import click
 
 from tika import parser
+from pprint import pformat
+
+def _parse_tika_output(fpath):
+    parsed = parser.from_file(fpath)
+    text = parsed['content'].strip()
+    meta = {
+        k.lower().replace('-', '_'): v for k, v in parsed['metadata'].items()
+        if k.lower() in ('author', 'content-type', 'creator', 'application-name')
+    }
+    return text, meta
 
 def _parse_stdout(stdout):
     lines = [line.strip() for line in stdout.decode().split('\n')]
     return json.loads(' '.join(lines))
 
-@click.command()
-@click.option(
-    '--fpath',
-    '-f',
-    type=click.Path(exists=True),
-    help='File path to analyze'
-)
-def check_pii(fpath):
-    # read document with tika and clean it ready for load
-    parsed = parser.from_file(fpath)
-    text = parsed['content'].strip()
-    meta = {
-        k: v for k, v in parsed['metadata'].items()
-        if k.lower() in ('author', 'content-type', 'creator', 'application-name')
-    }
-    # AWS comprehend entity detection
-    cmd = [
-        'aws', 'comprehend',
-        'detect-entities',
-        '--text', text,
-        '--language-code', 'en'
-    ]
-    process = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
-    output = _parse_stdout(process.stdout)
-    # clean output for loading
+def _parse_comprehend_output(output):
     entities = {}
     for entity in output['Entities']:
         entity_type = entity['Type']
@@ -47,10 +35,31 @@ def check_pii(fpath):
             entities[entity_type] = [
                 {k: v for k, v in entity.items() if k != 'Text'}
             ]
-    meta['entities'] = entities
-    import pprint
-    pprint.pprint(meta)
+    return entities
 
+@click.command()
+@click.argument(
+    'fpath',
+    type=click.Path(exists=True),
+)
+def check_pii(fpath):
+    fdir, fname = os.path.split(os.path.realpath(fpath))
+    text, meta = _parse_tika_output(fpath)
+    # AWS comprehend entity detection
+    cmd = [
+        'aws', 'comprehend',
+        'detect-entities',
+        '--text', text,
+        '--language-code', 'en'
+    ]
+    process = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
+    output = _parse_stdout(process.stdout)
+    # clean output for loading
+    meta['filename'] = fname
+    meta['file_dir'] = fdir
+    meta['entities'] = _parse_comprehend_output(output)
+    formatted_output = pformat(meta)
+    click.echo(formatted_output)
 
 if __name__ == '__main__':
     check_pii()
